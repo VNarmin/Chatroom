eaac69dae11f21a69993c0aa9276e23766a7f19e
VNarmin <Vnermin655@gmail.com>
2024-11-09 23:42:50 +0400

    commit

--- auth/chat_auth.py
+++ auth/chat_auth.py
@@ -1,26 +1,37 @@
 import mysql.connector
 
+# establishes a connection to the MySQL database using the mysql.connector library
+# connects to the database specified by the host, user, password and database parameters
+# in this case -- localhost, root, root and chatroom
+# the connection object returned can be used to interact with the database
 def get_db_connection() :
-    return (mysql.connector.connect (
+    return mysql.connector.connect (
         host = "localhost",
         user = "root",
         password = "root",
         database = "chatroom"
-    ))
+    )
 
+# responsible for registering a new user in the users table of the chatroom database
+# takes a username and a password as inputs and attempts to insert them into the users table
+# if the username is already in use (a duplicate entry), the function will catch the error and return False
+# Otherwise, the user is successfully registered and the function returns True
 def register_user(username, password) :
     conn = get_db_connection()
     cursor = conn.cursor()
     try :
         cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
         conn.commit()
-        return True  # registration successful
+        return True
     except mysql.connector.IntegrityError :
-        return False  # username already exists
+        return False
     finally :
         cursor.close()
         conn.close()
 
+# checks if a user exists in the database with the provided username and password
+# performs a SELECT query on the users table and returns True if a matching record is found
+# if no such record exists, the function returns False, indicating that the authentication has failed
 def authenticate_user(username, password) :
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -28,4 +39,4 @@ def authenticate_user(username, password) :
     result = cursor.fetchone()
     cursor.close()
     conn.close()
-    return result is not None  # returns True if username exists and password matches
+    return result is not None
